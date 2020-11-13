a5ab29f377d080543f1b7e8ef200f8ca1d4e1219
huyongsheng <tohys@qq.com>
2020-11-13 16:34:30 +0800

    add : 添加底部导航栏

---
+++ lib/demo/words.dart
@@ -0,0 +1,97 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_app/pigeons/messages.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestion = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = TextStyle(fontSize: 18);
+  final _batteryChannel = MethodChannel('com.example.flutter_app/battery');
+  int _batteryLevel = 0;
+  String _replay = '';
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: FlatButton(
+            child: Text('$_replay: $_batteryLevel'),
+            onPressed: _getBattery,
+          ),
+          actions: [IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)],
+        ),
+        body: _buildSuggestions(),
+      );
+
+  Widget _buildSuggestions() => ListView.builder(
+        padding: EdgeInsets.all(16),
+        itemBuilder: (context, i) {
+          if (i >= _suggestion.length) _suggestion.addAll(generateWordPairs().take(10));
+          return _buildRow(_suggestion[i]);
+        },
+      );
+
+  Widget _buildRow(WordPair pair) {
+    var alreadySaved = _saved.contains(pair);
+    return Column(
+      children: [
+        ListTile(
+          title: Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ),
+          trailing: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+          ),
+          onTap: () => _triggerSaved(pair, alreadySaved),
+        ),
+        Divider(),
+      ],
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      final tiles = _saved.map((pair) => ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+          ));
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Saved Suggestion"),
+        ),
+        body: ListView(
+          children: ListTile.divideTiles(context: context, tiles: tiles).toList(),
+        ),
+      );
+    }));
+  }
+
+  void _triggerSaved(WordPair pair, bool alreadySaved) => setState(() {
+        alreadySaved ? _saved.remove(pair) : _saved.add(pair);
+      });
+
+  Future<void> _getBattery() async {
+    try {
+      int result = await _batteryChannel.invokeMethod('getBattery');
+      SearchReplay replay = await Api().search(SearchRequest()..query = 'query');
+      setState(() {
+        _batteryLevel = result;
+        _replay = replay.result;
+      });
+    } on PlatformException {
+      setState(() {
+        _batteryLevel = -1;
+        _replay = '';
+      });
+    }
+  }
+}
---
+++ lib/home/home.dart
@@ -0,0 +1,118 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:animations/animations.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/demo/words.dart';
+
+enum BottomNavigationDemoType {
+  withLabels,
+  withoutLabels,
+}
+
+class BottomNavigationDemo extends StatefulWidget {
+  const BottomNavigationDemo({Key key, @required this.type}) : super(key: key);
+
+  final BottomNavigationDemoType type;
+
+  @override
+  _BottomNavigationDemoState createState() => _BottomNavigationDemoState();
+}
+
+class _BottomNavigationDemoState extends State<BottomNavigationDemo> {
+  int _currentIndex = 0;
+
+  String _title(BuildContext context) {
+    return '标题';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+
+    var bottomNavigationBarItems = <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: Icon(Icons.add_comment),
+        label: '注释',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.calendar_today),
+        label: '日历',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.account_circle),
+        label: '账号',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.alarm_on),
+        label: '闹钟',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.camera_enhance),
+        label: '相机',
+      ),
+    ];
+
+    if (widget.type == BottomNavigationDemoType.withLabels) {
+      bottomNavigationBarItems = bottomNavigationBarItems.sublist(0, bottomNavigationBarItems.length - 2);
+      _currentIndex = _currentIndex.clamp(0, bottomNavigationBarItems.length - 1).toInt();
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(_title(context)),
+      ),
+      body: Center(
+        child: PageTransitionSwitcher(
+          child: _NavigationDestinationView(
+            // Adding [UniqueKey] to make sure the widget rebuilds when transitioning.
+            key: UniqueKey(),
+            item: bottomNavigationBarItems[_currentIndex],
+          ),
+          transitionBuilder: (child, animation, secondaryAnimation) => FadeThroughTransition(
+            child: child,
+            animation: animation,
+            secondaryAnimation: secondaryAnimation,
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: widget.type == BottomNavigationDemoType.withLabels,
+        items: bottomNavigationBarItems,
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedFontSize: textTheme.caption.fontSize,
+        unselectedFontSize: textTheme.caption.fontSize,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        selectedItemColor: colorScheme.onPrimary,
+        unselectedItemColor: colorScheme.onPrimary.withOpacity(0.38),
+        backgroundColor: colorScheme.primary,
+      ),
+    );
+  }
+}
+
+class _NavigationDestinationView extends StatelessWidget {
+  _NavigationDestinationView({Key key, this.item}) : super(key: key);
+
+  final BottomNavigationBarItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    switch (item.label) {
+      case '注释':
+        return RandomWords();
+      default:
+        return Center(
+          child: Text(item.label),
+        );
+    }
+  }
+}
